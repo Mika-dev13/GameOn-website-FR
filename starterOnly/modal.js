80ffb3891879f3e2755e5037775b83bbc60d03de
Michael Chorro <michaelchorro.dev@gmail.com>
2021-12-24 19:13:35 +0100

    ajout de la validation de l'email

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,9 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+let form = document.querySelector("#reserveForm");
+
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -19,8 +22,35 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
 // close modal
 document.querySelector(".close")
-        .addEventListener("click", () => modalbg.style.display = "none")
+        .addEventListener("click", () => modalbg.style.display = "none");
+
+// Email validation
+
+form.email.addEventListener("change", function () {
+  validEmail(this);
+});
+
+const validEmail = function (e) {
+  let emailRegExp = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g');
+ 
+  let validEmailText = document.querySelector(".validEmailText");
+
+  if(emailRegExp.test(e.value)) {
+    validEmailText.textContent = "Adresse valide";
+    validEmailText.style.color = "green"; 
+    validEmailText.style.fontSize = "14px"; 
+  } else {
+    validEmailText.textContent = "Adresse non valide";
+    validEmailText.style.color = "red"; 
+    validEmailText.style.fontSize = "14px"; 
+  }
+};
+
+
+
+
 
 
